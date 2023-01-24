e4e9ea6e8f79d2bbece9ead5783485dca30d18e2
ashwin <123428347+veloc1tyy@users.noreply.github.com>
2023-01-24 12:27:33 +0000

    replaced the pointer with j instead of dual pointers

--- twodarrays.java
+++ twodarrays.java
@@ -6,7 +6,7 @@ public class twodarrays {
         int[] pop2 = { 1, 2, 3 };
         for (int i = 0; i < pop.length; i++) {
             for (int j = 0; j < pop[i].length; j++) {
-                System.out.print(Arrays.toString(pop[i][j]));
+                System.out.print(Arrays.toString(pop[j]));
             }
         }
 
