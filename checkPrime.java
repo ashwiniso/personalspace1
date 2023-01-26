cd8f48b5f820e99f37ac120c51b1c67cae2bb4a5
veloc1tyy <ashwinnanda@protonmail.com>
2023-01-26 15:12:12 +0000

    3:12pm

---
+++ checkPrime.java
@@ -0,0 +1,43 @@
+
+// Import statement:
+import java.util.ArrayList;
+
+class checkPrime {
+
+    // Add your methods here:
+    public checkPrime() {
+
+    }
+
+    public ArrayList<Integer> primenumbers(int[] array) {
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        boolean isprime = false;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 2; j < array[i]; j++) {
+                if (array[i] % j == 0) {
+                    isprime = false;
+                    break;
+                } else {
+                    isprime = true;
+                }
+            }
+            if (isprime) {
+                primes.add(array[i]);
+
+            }
+        }
+        return primes;
+
+    }
+
+    public static void main(String[] args) {
+
+        checkPrime pd = new checkPrime();
+        int[] numbers = { 6, 29, 28, 33, 11, 100, 101, 43, 89 };
+        System.out.println(pd.primenumbers(numbers));
+        for (int i = 0; i < numbers.length - 1; i++) {
+            System.out.println(test.sum(numbers[i], numbers[i + 1]));
+
+        }
+    }
+}
--- test.java
+++ test.java
@@ -1,43 +1,16 @@
+import java.util.*;
 
-// Import statement:
-import java.util.ArrayList;
+public class test {
 
-class test {
-
-    // Add your methods here:
-    public test() {
-
-    }
-
-    public ArrayList<Integer> primenumbers(int[] array) {
-        ArrayList<Integer> primes = new ArrayList<Integer>();
-        boolean isprime = false;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 2; j < array[i]; j++) {
-                if (array[i] % j == 0) {
-                    isprime = false;
-                    break;
-                } else {
-                    isprime = true;
-                }
-            }
-            if (isprime) {
-                primes.add(array[i]);
-
-            }
-        }
-        return primes;
-
+    public static int sum(int numberone, int numbertwo) {
+        return numberone + numbertwo;
     }
 
     public static void main(String[] args) {
-
-        test pd = new test();
-        int[] numbers = { 6, 29, 28, 33, 11, 100, 101, 43, 89 };
-        System.out.println(pd.primenumbers(numbers));
-        for (int i = 0; i < numbers.length - 1; i++) {
-            System.out.println(test3.sum(numbers[i], numbers[i + 1]));
-
-        }
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println(sum(a, b));
+        System.out.println(test4.name);
     }
 }
