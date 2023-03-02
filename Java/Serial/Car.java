b4e524765f89b70aeae2d7c1dfb03694b11bd307
Ashwin Nanda <2772570n@student.gla.ac.uk>
2023-03-02 16:12:24 +0000

    serial demo

--- Java/Serial/Car.java
+++ Java/Serial/Car.java
@@ -4,25 +4,54 @@ public class Car implements Serializable{
     private static final long serialVersionUID = 1L;
     private int year;
     private String make;
+    private transient String model;
 
 
-    public Car(String make, int year){
+    public Car(String make, int year, String model){
         this.make = make;
         this.year = year;
+        this.model = model;
+    }
+
+    public String toString(){
+        return "The car is made by " + make + ", in year = " + year + ",  of model = " + model;
     }
 
 
 
-    public static void main(String[] args) throws FileNotFoundException, IOException{
-        Car carone  = new Car("Mazda RX7", 1999);
-        Car cartwo  = new Car("Mitsubishi Eclipse GX '99", 1999);
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException{
+        Car mazda  = new Car("Mazda", 1999, "RX7");
+        Car mitsu  = new Car("Mitsubishi", 1999, "Eclipse GX '99");
 
         FileOutputStream fileOutputStream = new FileOutputStream("cars.png");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 
 
-        objectOutputStream.writeObject(carone);
-        objectOutputStream.writeObject(cartwo);
+        objectOutputStream.writeObject(mazda);
+        objectOutputStream.writeObject(mitsu);
+
+
+
+        //desiarlization
+        FileInputStream fileInputStream = new FileInputStream("cars.png");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        Car mazdaCopy = (Car) objectInputStream.readObject();
+        Car mitsuCopy = (Car) objectInputStream.readObject();
+
+        boolean isSameObject = mitsuCopy.equals(mitsu);
+
+        System.out.println("Mitsubishi Copy: " + mitsuCopy);
+        System.out.println("Mitsubishi Original: " + mitsu);
+        System.out.println("is same object: " + isSameObject);
+
+
+
+
+
+
+
+
 
 
     }
